e9fa5353b6240ef231202ae79aab72f87671c548
Oliver <2205456534@qq.com>
2019-05-22 23:12:55 +0800

    工作区初始化

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<style lang="stylus" rel="stylesheet/stylus">
+.mainApp
+  position absolute
+  height 100%
+  width 100%
+</style>
+
+<template>
+<div class="mainApp">
+  <router-view class="router-view"></router-view>
+</div>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  created() {}
+}
+
+</script>
